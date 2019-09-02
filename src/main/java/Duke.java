2f2968f2977c1acecc1a4bce425e771b2983d8ea
AmosChan97 <amoszzchan@outlook.com>
2019-09-02 15:43:41 +0800

    A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,7 +15,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
         fileToList();
 
         Scanner scanner = new Scanner(System.in);
